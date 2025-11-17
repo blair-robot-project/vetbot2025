21d27b3c0cedc274ec27609f8889853c35a883e8
Edem Setse <edemsetse@gmail.com>
2025-11-17 08:23:22 -0500

    cleanup so intellij doesnt scream at us

--- src/main/kotlin/frc/team449/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/team449/subsystems/intake/IntakeConstants.kt
@@ -1,15 +1,17 @@
 package frc.team449.subsystems.intake
 
 object IntakeConstants {
-  const val INTAKE_WHEEL_LEADER_ID = 0 // TODO: find
-  const val INTAKE_WHEEL_LEADER_INVERTED = false // TODO: find
-  const val INTAKE_WHEEL_FOLLOWER_ID = 1 // TODO: find
-  const val INTAKE_WHEEL_FOLLOWER_INVERTED_FROM_LEADER = true
+  const val WHEEL_LEADER_ID = 0 // TODO: find
+  const val WHEEL_LEADER_INVERTED = false // TODO: find
+  const val WHEEL_FOLLOWER_ID = 1 // TODO: find
+  const val WHEEL_FOLLOWER_INVERSION = true
   const val ROLLER_ID = 2 // TODO: find
   const val ROLLER_INVERTED = false // TODO: find
 
-  const val INTAKE_WHEEL_INTAKE_VOLTAGE = 4.0
-  const val INTAKE_WHEEL_OUTTAKE_VOLTAGE = -6.0
+  const val SENSOR_ID = 3 // TODO: find
+
+  const val WHEEL_INTAKE_VOLTAGE = 4.0
+  const val WHEEL_OUTTAKE_VOLTAGE = -6.0
   const val ROLLER_INTAKE_VOLTAGE = 4.0
   const val ROLLER_OUTTAKE_VOLTAGE = 6.0
 }
--- src/main/kotlin/frc/team449/subsystems/intake_pivot/IntakePivot.kt
+++ src/main/kotlin/frc/team449/subsystems/intake_pivot/IntakePivot.kt
@@ -4,52 +4,49 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.hardware.TalonFX
 import com.ctre.phoenix6.signals.GravityTypeValue
 import com.ctre.phoenix6.signals.NeutralModeValue
-import com.revrobotics.spark.SparkMax
 import edu.wpi.first.epilogue.Logged
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team449.system.motor.createFollowerSpark
 import frc.team449.system.motor.createKraken
-import frc.team449.system.motor.createSparkMax
 import edu.wpi.first.units.Units.*
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.wpilibj2.command.Command
 
 @Logged
 class IntakePivot (
-  val pivot_motor: TalonFX,
+  val pivotMotor: TalonFX,
 ) : SubsystemBase() {
 
   fun setPosition(position: Angle): Command {
     return runOnce {
-      pivot_motor.setPosition(position.`in`(Rotations))
+      pivotMotor.setPosition(position.`in`(Rotations))
     }
   }
 
   fun stop(): Command {
     return runOnce {
-      pivot_motor.stopMotor()
+      pivotMotor.stopMotor()
     }
   }
 
   fun coast(): Command {
     return runOnce {
-      val new_config = TalonFXConfiguration()
-      new_config.MotorOutput.NeutralMode = NeutralModeValue.Coast
-      pivot_motor.configurator.apply(new_config)
+      val newConfig = TalonFXConfiguration()
+      newConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast
+      pivotMotor.configurator.apply(newConfig)
     }
   }
 
   fun brake(): Command {
     return runOnce {
-      val new_config = TalonFXConfiguration()
-      new_config.MotorOutput.NeutralMode = NeutralModeValue.Brake
-      pivot_motor.configurator.apply(new_config)
+      val newConfig = TalonFXConfiguration()
+      newConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake
+      pivotMotor.configurator.apply(newConfig)
     }
   }
 
   companion object {
     fun createIntakePivot() {
-      val pivot_motor = createKraken(
+      val pivotMotor = createKraken(
         IntakePivotConstants.PIVOT_ID,
         IntakePivotConstants.PIVOT_INVERTED,
         kS = IntakePivotConstants.PIVOT_KS,
