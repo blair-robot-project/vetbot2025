3797383350b41ac056b160729e01674d077731ca
Tinbite-A <tinbite724@gmail.com>
2025-12-05 18:12:24 -0500

    actually deleted vision stuff out

--- src/main/kotlin/frc/team449/subsystems/vision/interpolation/VisionInterpolationData.kt
+++
@@ -1,8 +0,0 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-package frc.team449.subsystems.vision.interpolation
-
-import edu.wpi.first.math.geometry.Translation2d
-
-data class VisionInterpolationData(val measuredPose: Translation2d, val visionPose: Translation2d, val label: String)
